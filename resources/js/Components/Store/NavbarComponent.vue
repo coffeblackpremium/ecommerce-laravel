af8c7f6fbadf86391220448474d70079b1c67935
João Damaceno <joaodamaceno188@gmail.com>
2022-10-21 14:22:15 -0400

    feat: funcionalidade de navbar e flex adicionado

---
+++ resources/js/Components/Store/NavbarComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+    <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white
+    shadow sm:items-baseline w-full">
+        <div class="mb-2 sm:mb-0">
+            <a href="#home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark">Home</a>
+        </div>
+    </nav>
+</template>
--- resources/js/Pages/Store/StoreTeste.vue
+++ resources/js/Pages/Store/StoreTeste.vue
@@ -1,14 +1,28 @@
 
 <template>
-    <li v-for="key in info">
-        <h1>{{key.image}}</h1>
-    </li>
+    <Navbar></Navbar>
+    <div class="container mx-auto px-4 mt-9">
+        <div class="flex flex-row flex-wrap justify-center mx-auto">
+            <li v-for="key in info" class="px-4 box-border list-none">
+               <div class="shadow mx-5 mt-5 py-3">
+                <a href="#">
+                    <img :src="key.image" :style="{width: `150px`, height: `150px`}" class="px-4 "/>
+                </a>
+                   <span class="ml-3">Produto:</span>
+                   <span><strong>R$: 15,00</strong></span>
+               </div> 
+            </li>
+        </div>
+
+    </div>
     
 </template>
 
 
 <script>
 import axios from 'axios'
+import Navbar from '@/Components/Store/NavbarComponent.vue';
+
 export default {
     data(){
         return {
@@ -21,6 +35,9 @@ export default {
         .then(response => (this.info = response.data))
         .catch(error => console.log(error))
     },
+    components: {
+        Navbar
+    }
 }
 
 </script>
